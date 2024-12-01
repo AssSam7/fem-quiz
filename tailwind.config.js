37c002dd5ccfbc08f2a4c7a7c3930ddc3df11fe3
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2024-12-01 13:59:11 +0530

    Adding the base colors, typography elements

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+    },
+    fontSize: {
+      sm: ["12px", "150%"],
+      base: ["28px", "100%"],
+      lg: ["36px", "100%"],
+      "2xl": ["64px", "100%"],
+      "3xl": ["144px", "100%"],
+    },
+    fontFamily: {
+      rubik: ["Rubik", "sans-serif"],
+    },
+    colors: {
+      purple: "#A729F5",
+      "dark-navy": "#313E51",
+      navy: "#3B4D66",
+      "grey-navy": "#626C7F",
+      "light-grey": "#ABC1E1",
+      "light-bluish": "#F4F6FA",
+      "pure-white": "#FFFFFF",
+      green: "##26D782",
+      red: "#EE5454",
+    },
+    extend: {},
+    container: {
+      center: true,
+    },
+  },
+  plugins: [],
+};
